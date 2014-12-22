a7c31dcbc9e3bb97a9e0ccc209c263008e32b294
Will Smith <github@septemberlabs.com>
2014-12-22 18:42:18 -0500

    Initial Commit

---
+++ UrbanTurf/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  UrbanTurf
+//
+//  Created by Will Smith on 12/22/14.
+//  Copyright (c) 2014 Will Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
---
+++ UrbanTurf/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UrbanTurf
+//
+//  Created by Will Smith on 12/22/14.
+//  Copyright (c) 2014 Will Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
